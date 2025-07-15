d6e2c89b290a929d02d6b7d948e65a4a765ff6f0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-15 19:27:59 +0100

    new

--- chapter4/cmd/TaskOne/main.go
+++ chapter4/cmd/TaskOne/main.go
@@ -1,1 +1,16 @@
-package TaskOne
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`
+	(a) Sequence, selection and iteration,
+	(b) 'if' and 'else' statement,
+	(c) definite control,
+	(d) sentinel, 
+	(e) sequence structure,
+	(f) zero,
+	(g) prefix,
+	(h) 8`)
+}
