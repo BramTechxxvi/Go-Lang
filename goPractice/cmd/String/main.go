e8dbe5e73298df9016e8a9e3dc20f6c543a39a15
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-15 11:26:17 +0100

    new

---
+++ goPractice/cmd/String/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello world \n")
+	fmt.Println("Today is bright")
+
+	//var content String = "I have a couple of bugs to fix"
+	//fmt.Printf("%s\n", content)
+
+}
