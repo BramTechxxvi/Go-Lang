06d47795e22055599865fb6a65d59516d03653df
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-15 11:20:57 +0100

    new

--- goPractice/cmd/Operators/main.go
+++ goPractice/cmd/Operators/main.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world \n")
-	fmt.Println("Today is bright")
-
-	//var content String = "I have a couple of bugs to fix"
-	//fmt.Printf("%s\n", content)
-
 	var firstNumber = 10
 	var secondNumber = 121
 	var thirdNumber = 390
