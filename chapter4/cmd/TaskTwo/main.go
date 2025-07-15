67f5e959e6a21f2f92b9440c938afe9cf68dbc95
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-15 19:33:20 +0100

    new

--- chapter4/cmd/TaskTwo/main.go
+++ chapter4/cmd/TaskTwo/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fmt.Println(`
+	(a) true,
+	(b) true,
+	(c) false, a selection statement is used to decide if a condition is true or false,
+	(d) sentinel, 
+	(e) sequence structure,
+	(f) zero,
+	(g) prefix,
+	(h) 8`)
 }
