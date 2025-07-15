86581dc82c1d3d4aaadfb7b4b3dd781932b92abb
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-15 11:12:53 +0100

    new

--- goPractice/cmd/Operators/main.go
+++ goPractice/cmd/Operators/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Print("Hello world \n")
+	fmt.Println("Today is bright")
+
+	//var content String = "I have a couple of bugs to fix"
+	//fmt.Printf("%s\n", content)
+
+	var firstNumber = 10
+	var secondNumber = 121
+	var thirdNumber = 390
+	var fourthNumber = 987
+
+	fmt.Println(addUp())
+	fmt.Println(firstNumber - secondNumber)
+	fmt.Printf("The result is: %d ", thirdNumber/30)
+	fmt.Printf()
+}
+
+func addUp(first int, second int, rest ...int) int {
+	var sum = first + second
+
+	for _, value := range rest {
+		sum += value
+	}
+	return sum
 }
