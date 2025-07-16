e7bc59adeeb06e90eb4e34d1050cfce2621e69de
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 05:49:35 +0100

    new

--- chapter4/TaskEight/main.go
+++ chapter4/TaskEight/main.go
@@ -1,1 +1,10 @@
-package TaskEight
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`
+	(a) no while loop in GO.
+    (b) remove semicolon after else statement and brackets in if condition. `)
+}
--- chapter4/TaskNine/main.go
+++ chapter4/TaskNine/main.go
@@ -1,1 +1,8 @@
-package TaskNine
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("No while loop in GO")
+}
