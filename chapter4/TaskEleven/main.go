1294eaab8d10e9d787c200f014533f3933f2dd3d
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 06:10:53 +0100

    new

--- chapter4/TaskEleven/main.go
+++ chapter4/TaskEleven/main.go
@@ -1,1 +1,8 @@
-package TaskEleven
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("When you divide an integer with another in Go \n the fractional part is truncated \n to avoid that convert one of the operands to float before dividing")
+}
