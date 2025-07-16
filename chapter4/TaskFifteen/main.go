dcc5a25e9fcab1e44c5f9fb65ea1cc15b5c4d345
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 06:38:24 +0100

    new

--- chapter4/TaskFifteen/main.go
+++ chapter4/TaskFifteen/main.go
@@ -1,1 +1,12 @@
-package TaskFifteen
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`
+	(a) brackets are not allowed in if condition, no need for semicolon also missing curly braces in the if and else block, also a quote after the bracket in else statement
+	(b) no while loop in GO
+	(c) no while loop on Go
+	(d) no while iteration statement in GO`)
+}
