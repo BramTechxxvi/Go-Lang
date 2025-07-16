62cfea45fcb65c143e860f85f321c4d90c9a7e89
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 06:24:09 +0100

    new

--- chapter4/TaskThirteen/main.go
+++ chapter4/TaskThirteen/main.go
@@ -1,1 +1,16 @@
-package TaskThirteen
+package main
+
+import "fmt"
+
+func main() {
+
+	var userInput int
+	for {
+		fmt.Println("Enter")
+		fmt.Scan(&userInput)
+		if userInput == 0 {
+			break
+		}
+		fmt.Println(userInput)
+	}
+}
