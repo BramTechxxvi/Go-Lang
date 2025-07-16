afc395d8667790da516495b0743ae3ad4688db28
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 09:17:34 +0100

    new

--- Diary/src/entry.go
+++ Diary/src/entry.go
@@ -17,3 +17,19 @@ func newEntry(id int, title string, body string) Entry {
 		dateCreated: time.Now(),
 	}
 }
+
+func (e *Entry) GetID() int {
+	return e.iD
+}
+
+func (e *Entry) setID(id int) {
+	e.iD = id
+}
+
+func (e *Entry) GetTitle() string {
+	return e.title
+}
+
+func (e *Entry) SetTitle(title string) {
+	e.title = title
+}
