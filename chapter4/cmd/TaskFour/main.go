a3c9f271073fe24f445e73c18b4f686fa6cd9536
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-16 05:20:43 +0100

    new

--- chapter4/cmd/TaskFour/main.go
+++ chapter4/cmd/TaskFour/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var x = 1
-	var y = 2
-	var z = x + y + 1
-	fmt.Println(z)
+
+	fmt.Println(`
+	(a) z = x + y + 1,
+	(b) if(count > 10 ) {
+		print(count) },
+	(c) total -= x--
+	(d) q = q % divisor
+		q %= divisor `)
 }
