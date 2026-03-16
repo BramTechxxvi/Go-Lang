3e10f1d03884ec9d919b387648788d5787b852aa
BramTechxxvi <bramtechxxvi@gmail.com>
2026-03-16 04:57:10 +0100

    fixed

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module go-lang
 
 go 1.26.1
-
-require github.com/01-edu/z01 v0.1.0
--- piscine/test/main.go
+++ piscine/test/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	golang "go-lang"
+	"piscine"
 )
 
 func main() {
-	golang.Only1()
+	piscine.Only1()
 }
