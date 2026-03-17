21be00c575358276f9bc62fc5ec07940f28156db
BramTechxxvi <bramtechxxvi@gmail.com>
2026-03-17 09:05:07 +0100

    fishandchips

---
+++ piscine/fishandchips.go
@@ -0,0 +1,17 @@
+package piscine
+
+func FishAndChips(n int) string {
+	result := ""
+	if n%2 == 0 && n%3 == 0 {
+		result = "fish and chips"
+	} else if n%3 == 0 {
+		result = "chips"
+	} else if n%2 == 0 {
+		result = "fish"
+	} else if n < 0 {
+		result = "error: number is negative"
+	} else if n%2 != 0 && n%3 != 0 {
+		result = "error: no divisible"
+	}
+	return result
+}
